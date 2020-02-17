6b48de23c786f0cffeba13f72024f0ef33cc9f6d
Francisco <bolanosf@oregonstate.edu>
2020-02-16 21:26:01 -0800

    Fitness function added to RiskCalc.py

--- Library/RiskCalc.py
+++ Library/RiskCalc.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from Library.DataClass import *
+import math
 
 
 # obtain data for each asset and return the historical data and weight of each asset along with the portfolio's
@@ -60,3 +61,115 @@ def value_at_risk(portf: Portfolio, conf_level: float, horizon: int):
     # returns Value at Risk as a value and as a percentage of total investment
     return vr,round(vr/investment,2)*100
 
+
+
+
+################ For genetic algorithm #################################
+
+# This function returns a pandas dataframe of all the historical prices of all the
+# assets in the universe. price_indicator is a string that indicates what column in
+# the orginal .csv file we want to use for calculating returns. i.e 'open','close',
+# 'adj. close', etc.
+def gen_universe_hist_data(universe:[Asset], price_indicator:str):
+    historical_data = pd.DataFrame()
+
+    # Empty chromosomes
+    if len(universe) == 0:
+        print("error: empty list")
+        return 0
+
+    for asset in universe:
+        # get data and reformat
+        asset_data = pd.read_csv(asset.price_history_file)
+        col = list(asset_data.columns)
+        col.remove(price_indicator)
+        col.remove('Date')
+        asset_data = asset_data.drop(columns=col)
+        asset_data = asset_data.rename(columns={price_indicator: asset.ticker})
+        asset_data = asset_data.set_index('Date')
+        asset_data = asset_data.dropna()
+
+        # join new asset data to overall data table
+        if historical_data.empty:
+            historical_data = asset_data
+        elif len(historical_data) <= len(asset_data):
+            historical_data = historical_data.join(asset_data)
+        else:
+            historical_data = asset_data.join(historical_data)
+
+    return historical_data
+
+
+# returns value at risk for genetic algorithm
+# weight array and universe must be 1 to 1
+# confidence level .9,.95,.99
+# reruns dataframe is a joint table of returns for all assets in the universe
+def gen_value_at_risk(weights: [float], returns: pd.DataFrame, conf_level: float):
+
+    # VaR Historical (another way to calculate VaR)
+    # data is total_returns
+    #indx = round((1-conf_level)*len(data.index)) # Percentile calculation
+    #return data.iloc[indx]
+
+    # VaR parametric
+    w = np.array(weights)
+    mean = returns.mean().dot(weights)
+    std = np.sqrt(w.T.dot(returns.cov()).dot(weights))
+
+    # parametric calculations
+    # there's a (1-conf_level)% probability that we loss at least
+    # x percent of our total portfolio value in a day
+    x = norm.ppf((1 - conf_level), mean, std)
+
+    # returns Value at Risk as a value and as a percentage of total investment
+    return round(x, 4)
+
+
+# This function returns the Renyi entropy given weights and joint table of returns
+# for all assets in the universe (data).
+# uses square root rule for determining number of subintervals
+def gen_entropy(weights: [float], data: pd.DataFrame):
+    n = data.shape[0]
+    k = int(round(np.sqrt(n)))
+    min = round(data.iloc[0],4)
+    bin_width = round((data.iloc[-1] - data.iloc[0])/k,4)
+    summation = 0
+
+    for i in range(1,k):
+        seriesObj = data.apply(lambda x: True if (min + bin_width * i) >= x > (min + bin_width * (i - 1)) else False)
+        numOfRows = len(seriesObj[seriesObj == True].index)
+        summation = summation + bin_width*(numOfRows/(n*bin_width))**2 # renyi calculation
+    return -round(math.log(summation,2),4) # renyi calculation
+
+
+# returns the probability that our portfolio daily return is 0% or greater.
+# uses normal parametric approximation
+def gen_pos_returns(weights: [float], returns: pd.DataFrame):
+    w = np.array(weights)
+    mean = returns.mean().dot(weights)
+    std = np.sqrt(w.T.dot(returns.cov()).dot(weights))
+
+    # parametric calculations
+    return 1 - norm.cdf(0, mean, std)
+
+
+# returns fitness score for a particular individual given its
+# weights(chromosome) and universe for which does weights are 1-1 with.
+def gen_fitness_value(weights: [float], universe: [Asset]):
+
+    # universe data
+    # speeds up if outside function and pass as parameter instead
+    # global variable?
+    historical_data= gen_universe_hist_data(universe,"Adj. Close")
+
+    # portfolio returns based on weights
+    returns = historical_data.pct_change()
+    total_returns = (returns.dropna()) * weights
+    total_returns = total_returns.apply(np.sum, axis=1).sort_values()
+
+    # total_returns have been sorted in ascending order
+    VaR = gen_value_at_risk(weights,returns,.95)
+    entropy = gen_entropy(weights,total_returns)
+    prob_pos_returns = gen_pos_returns(weights,returns)
+    #print(VaR,-entropy,prob_pos_returns)
+    return VaR*(-entropy)+(-entropy)+((-entropy)*prob_pos_returns)
--- main.py
+++ main.py
@@ -4,29 +4,23 @@ from Library.ReadUniverse import *
 from Library.FilterUniverse import *
 import os
 
-#example
-# os.path.abspath("/Stock_Data/TSLA.csv")
-# TSLA = Asset('TSLA',
-#             "Tesla",
-#              "stock",
-#              os.path.abspath("Stock_Data/TSLA.csv"),
-#              3
-#              )
-# GOOG = Asset('GOOG',
-#             "Alphabet",
-#              "stock",
-#              os.path.abspath("Stock_Data/GOOG.csv"),
-#              30
-#              )
+AKM = Asset(ticker='AKAM', name='Akamai Technologies Inc', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/AKAM.csv')
+CSRA = Asset(ticker='CSRA', name='CSRA Inc.', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/CSRA.csv')
+MCHP = Asset(ticker='MCHP', name='Microchip Technology', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/MCHP.csv')
+NKTR = Asset(ticker='NKTR', name='Nektar Therapeutics', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/NKTR.csv')
+#Asset(ticker='NFLX', name='Netflix Inc.', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/NFLX.csv'),
+#Asset(ticker='NRG', name='NRG Energy', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/NRG.csv'),
+#Asset(ticker='PCG', name='PG&E Corp.', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/PCG.csv'),
+#Asset(ticker='STX', name='Seagate Technology', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/STX.csv'),
+#Asset(ticker='XL', name='XL Capital', asset_type='stock', price_history_file='C:\\Users\\Francisco\\Documents\\AlgoTradingCode\\portfolio_gen\\Stock_Data/XL.csv')]
+
+print(gen_fitness_value([.6,.4,0,0], [AKM,CSRA,MCHP,NKTR]))
+print(gen_fitness_value([.4,.3,.2,.1], [AKM,CSRA,MCHP,NKTR]))
+print(gen_fitness_value([.9,.1,.0,.0], [AKM,CSRA,MCHP,NKTR]))
 
 #example
-# portf1 = Portfolio(1,30000,0,0,[TSLA,GOOG])
-# VaR_value,VaR_perc =value_at_risk(portf1,.95,1)
-# print(VaR_value,VaR_perc)
-
-#example
-universe = ReadUniverse()
-print(universe.count)
-filtered_universe = filter_universe(universe)
-print(filtered_universe.universe_set)
-print(filtered_universe.count)
+#universe = ReadUniverse()
+#print(universe.count)
+#filtered_universe = filter_universe(universe)
+#print(filtered_universe.universe_set)
+#print(filtered_universe[0])
